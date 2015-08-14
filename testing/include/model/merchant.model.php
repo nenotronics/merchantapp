4cd74f6dca0eb0077f7ee10f52b5e6f00cee5e11
Frans S Shafuda <franskafula@users.noreply.github.com>
2015-08-14 18:20:12 +0100

    Update merchant.model.php

--- testing/include/model/merchant.model.php
+++ testing/include/model/merchant.model.php
@@ -27,7 +27,7 @@ class Mechant{
 		}
 		else if(isset($arr['keyword'])){
 			$st = $db->prepare("SELECT * FROM merchant WHERE name like :keyword");
-		}
+		}   
 		else{ throw new Exception("Unsupported property!");}
 		$st->execute($arr);
 		// Returns an array of User objects:
@@ -42,22 +42,34 @@ class Mechant{
 				                       country_code, postal_address, telephone, cellphone, fax, email, website, 
 				                       contact_person, bank_account_holder_name, bank_name, bank_branch_name, 
 				                       bank_branch_code, bank_account_number, bank_account_type, bank_card_pan, 
-				                       bank_card_expiry_date, bank_card_cvv2, tresury_reference, opened_date, 
+				                       bank_card_expiry_date, bank_card_cvv2, treasury_reference, opened_date, 
 				                       closed_date, closed_reason, narrative_msg_type) 
-				VALUES ()"
+				VALUES 	(:name,:status, :addtional_reg, :vat_reg_no, :institution_id, :merchant_type_id, 
+			                       :head_card_acceptor, :key_name, :any_terminals, :physical_address, :state_code,
+			                       :country_code, :postal_address, :telephone, :cellphone, :fax, :email, :website, 
+			                       :contact_person, :bank_account_holder_name, :bank_name, :bank_branch_name, 
+			                       :bank_branch_code, :bank_account_number, :bank_account_type, :bank_card_pan, 
+			                       :bank_card_expiry_date, :bank_card_cvv2, :treasury_reference, :opened_date, 
+			                       :closed_date, :closed_reason, :narrative_msg_type)"
 		);
 		$st->execute($arr);
 	}
 	
-	public static function remove_merchant($arr = array()){
+	public static function remove_user($arr = array()){
 		global $db;
-		$st = $db->prepare("DELETE from merchant WHERE id=:id");
-		$st->execute($arr); 
+		$st = $db->prepare("DELETE from user WHERE username=:username");
+		$st->execute($arr);
 	}
 	
-	public static function update_user($arr = array()){
+	public static function update_merchant($arr = array()){
 		global $db;
-		$st = $db->prepare("UPDATE user SET password=:password, fname=:fname, lname=:lname, role_id=:role_id WHERE username=:username");
+		$st = $db->prepare("UPDATE merchant SET name=:name, status=:status, addtional_reg=:addtional_reg, vat_reg_no=:vat_reg_no, institution_id=:institution_id, merchant_type_id=:merchant_type_id, 
+				                       head_card_acceptor=:head_card_acceptor, key_name=:key_name, any_terminals=:any_terminals, physical_address=:physical_address, state_code=:state_code,
+				                       country_code=:country_code, postal_address=:postal_address, telephone=:telephone, cellphone=:cellphone, fax=:fax, email=:email, website=:website, 
+				                       contact_person=:contact_person, bank_account_holder_name=:bank_account_holder_name, bank_name=:bank_name, bank_branch_name=:bank_branch_name, 
+				                       bank_branch_code=:bank_branch_code, bank_account_number=:bank_account_number, bank_account_type=:bank_account_type, bank_card_pan=:bank_card_pan, 
+				                       bank_card_expiry_date=:bank_card_expiry_date, bank_card_cvv2=:bank_card_cvv2, treasury_reference=:treasury_reference, opened_date=:opened_date, 
+				                       closed_date=:closed_date, closed_reason=:closed_reason, narrative_msg_type=:narrative_msg_type WHERE id=:id");
 		$st->execute($arr);
 	}
 }
