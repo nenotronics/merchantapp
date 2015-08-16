84c3104c97556e9420e5d79201660d5088dbc7ef
kennethgideon <kennethgideon@gmail.com>
2015-08-16 16:22:27 +0100

    Create main.php

---
+++ testing/include/main.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+	This is the main include file.
+	It is used in index.php and keeps it much cleaner.
+*/
+
+require_once "include/connect.php";
+/************************** The data models *********************************/
+require_once "include/model/user.model.php";
+
+/********************* The action controllers ******************************/
+
+?>
